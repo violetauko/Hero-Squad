b891f7a04cda8b75e88343087ec562ab311f43a0
violetauko <aukoviolet24@gmail.com>
2022-05-02 21:51:06 +0300

    created hero-detail file where created heroes are viewed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
+import models.Hero;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import static spark.Spark.*;
@@ -20,6 +22,13 @@ public class App {
         get("/hero-form",(request, response) ->{
             Map<String, Object> model = new HashMap<>();
             return new ModelAndView(model, "hero-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/hero-detail",(req, res) ->{
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Hero> hero = Hero.getAll();
+            model.put("hero",hero);
+            return new ModelAndView(model, "hero-detail.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
