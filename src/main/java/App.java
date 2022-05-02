647374042d85e4624c6f8dd59b79d0166d2b82c7
violetauko <aukoviolet24@gmail.com>
2022-05-02 21:31:40 +0300

    added home page route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,20 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+import static spark.Spark.*;
+
 import static spark.Spark.staticFileLocation;
 
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model,"index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
