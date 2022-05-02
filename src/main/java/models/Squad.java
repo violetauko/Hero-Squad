03f6c1ff76e5de805e2a3724b9b0d3bbbbfc1337
violetauko <aukoviolet24@gmail.com>
2022-05-02 19:03:16 +0300

    created squad class

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,4 +1,64 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Squad {
-}
+
+    private String name;
+    private String cause;
+    private int size;
+    private int SquadId;
+    private static ArrayList<Squad> instances = new ArrayList<>();
+    private static ArrayList<Hero> member = new ArrayList<>();
+
+    public Squad(String name, String cause, int size) {
+        this.name = name;
+        this.cause = cause;
+        this.size = size;
+        this.member = new ArrayList<>();
+        instances.add(this);
+        this.SquadId = instances.size();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getSquadId() {
+        return SquadId;
+    }
+
+    public static ArrayList<Squad> getInstances() {
+        return instances;
+    }
+
+    public static void setMember(Hero newMember) {
+        member.add(newMember);
+    }
+
+    public static ArrayList<Hero> getMember() {
+        return member;
+    }
+    public static void clearAllSquad(){
+        instances.clear();
+    }
+    public static Squad findById(int id){
+        return instances.get(id-1);
+    }
+    public void clearAllMember(){
+        getMember().clear();
+    }
+
+    public static Squad setUpNewSquad(){
+        return new Squad("Justice","Justice",10);
+    }
+
+    }
---
+++ src/test/java/models/SquadTest.java
@@ -0,0 +1,54 @@
+package models;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SquadTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getCause() {
+    }
+
+    @Test
+    void getSize() {
+    }
+
+    @Test
+    void getSquadId() {
+    }
+
+    @Test
+    void getInstances() {
+    }
+
+    @Test
+    void setMember() {
+    }
+
+    @Test
+    void getMember() {
+    }
+
+    @Test
+    void clearAllSquad() {
+    }
+
+    @Test
+    void findById() {
+    }
+}
