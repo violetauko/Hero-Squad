b6c510f6f4fffce920b6593fd7bedf9955219df0
violetauko <aukoviolet24@gmail.com>
2022-05-02 23:28:13 +0300

    created a squad form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,5 @@
 import models.Hero;
+import models.Squad;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -24,11 +25,52 @@ public class App {
             return new ModelAndView(model, "hero-form.hbs");
         }, new HandlebarsTemplateEngine());
 
-        get("/hero-detail",(req, res) ->{
+        get("/hero-detail", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
             ArrayList<Hero> hero = Hero.getAll();
+            model.put("hero", hero);
+            return new ModelAndView(model, "hero-detail.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/hero/new",(req, res) ->{
+            Map<String, Object> model = new HashMap<>();
+            String name = req.queryParams("name");
+            Integer age = Integer.parseInt(req.queryParams("age"));
+            String specialPower = req.queryParams("specialPower");
+            String weakness = req.queryParams("weakness");
+            Hero hero = new Hero(name,age,specialPower,weakness);
+            req.session().attribute("item",name);
+            model.put("item",req.session().attribute("item"));
             model.put("hero",hero);
             return new ModelAndView(model, "hero-detail.hbs");
         }, new HandlebarsTemplateEngine());
+
+        get("/squad-form",(req, res) ->{
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "squad-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/squad-detail",(req, res) ->{
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Squad> squads = Squad.getInstances();
+            model.put("squad",squads);
+            ArrayList<Hero> member = Hero.getAll();
+            model.put("hero",member);
+            Squad squad = Squad.findBySquadId(1);
+            model.put("member", squad.getMember());
+            return new ModelAndView(model, "squad-detail.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/squad/new",(request,response)-> {
+            Map<String, Object> model = new HashMap<>();
+            String name = request.queryParams("name");
+            Integer size = Integer.parseInt(request.queryParams("size"));
+            String cause = request.queryParams("cause");
+            Squad squad = new Squad(name,cause,size);
+            request.session().attribute("item",name);
+            model.put("item",request.session().attribute("item"));
+            return new ModelAndView(model,"squad-detail.hbs");
+        }, new HandlebarsTemplateEngine());
+
     }
 }
